55b3186d90c3d50746e0e28db58f03ff3a9f4211
estherkim0109 <dptmej0501@naver.com>
2025-09-10 11:39:09 +0900

    1차완료

---
+++ src/main/java/io/goorm/mybatisboard/mapper/PostMapper.java
@@ -0,0 +1,21 @@
+package io.goorm.mybatisboard.mapper;
+
+import io.goorm.mybatisboard.model.Post;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PostMapper {
+    
+    List<Post> findAll();
+    
+    Post findById(@Param("id") Long id);
+    
+    void save(Post post);
+    
+    void update(@Param("id") Long id, @Param("post") Post post);
+    
+    void delete(@Param("id") Long id);
+}
